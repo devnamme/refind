901d029520f2810d02608788582301bfc30c7ed6
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2023-04-18 07:07:24 +0800

    add base files

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color PRIMARY_MAIN = Color(0xFF000000);
+}
---
+++ lib/screens/splash_page.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Text('Splash Page'),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, route);
+  }
+
+  route() {
+    // Navigator.of(context)
+    //     .pushReplacement(MaterialPageRoute(builder: (_) => NextPage()));
+  }
+}
